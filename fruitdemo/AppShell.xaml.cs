ceee14c082145344c47900677d2fdb1fdf2277ed
Eli Porcel <114632682+EliPorcel@users.noreply.github.com>
2024-06-08 15:57:20 -0300

    Add project files.

---
+++ fruitdemo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace fruitdemo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ fruitdemo/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using fruitdemo.Models;
+
+namespace fruitdemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<FruitInformation> PapayaInfo { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            PapayaInfo = new List<FruitInformation>
+            {
+                new FruitInformation
+                {
+                    Micronutrient = "Vitamina C",
+                    Percentage = 10
+                },
+                new FruitInformation
+                {
+                    Micronutrient = "Vitamina A",
+                    Percentage = 19
+                },
+                new FruitInformation
+                {
+                    Micronutrient = "Calcio",
+                    Percentage = 2
+                },
+                new FruitInformation
+                {
+                    Micronutrient = "Magnesio",
+                    Percentage = 5
+                },
+                new FruitInformation
+                {
+                    Micronutrient = "Potasio",
+                    Percentage = 5
+                }
+            };
+            this.BindingContext = this;
+        }
+    }
+}
